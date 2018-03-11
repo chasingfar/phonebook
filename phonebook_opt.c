81cef16765ae3755016c6d717bcea21ebf2bd815
chasingfar <chasingfar@gmail.com>
2018-03-11 22:06:16 +0800

    Use big index to fit CPU's cache size

--- phonebook_opt.c
+++ phonebook_opt.c
@@ -1,27 +1,49 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 #include "phonebook_opt.h"
 
 /* TODO: FILL YOUR OWN IMPLEMENTATION HERE! */
-entry *findName(char lastName[], entry *pHead)
+record *findName(char lastName[], entry *pHead)
 {
-    /* TODO: implement */
+    while (pHead != NULL) {
+        for(int i=0;i<INDEX_SIZE;i++){
+            if (strcasecmp(lastName, pHead->lastName[i]) == 0)
+                return pHead->pRecord[i];
+        }
+        pHead = pHead->pNext;
+    }
     return NULL;
 }
 
 entry *append(char lastName[], entry *e)
 {
-    /* TODO: implement */
-    return NULL;
+    if(e->length>=INDEX_SIZE){
+        e->pNext = (entry *) malloc(sizeof(entry));
+        e->length=0;
+        e=e->pNext;
+        e->pNext=NULL;
+    }
+    e->pRecord[e->length]=(record *) malloc(sizeof(record));
+    strcpy(e->pRecord[e->length]->lastName, lastName);
+    strcpy(e->lastName[e->length], lastName);
+    e->length++;
+
+    return e;
 }
 
 entry *init()
 {
-    /* TODO: implement */
-    return NULL;
+    entry *e = (entry *) malloc(sizeof(entry));
+    e->length=0;
+    e->pNext=NULL;
+    printf("size of INDEX : %d\n", INDEX_SIZE);
+    return e;
 }
 
 void clean(entry *pHead)
 {
-    /* TODO: implement */
+    if(pHead->pNext)free(pHead->pNext);
+    free(pHead);
 }
--- phonebook_opt.h
+++ phonebook_opt.h
@@ -5,9 +5,15 @@
 
 /* TODO: After modifying the original version, uncomment the following
  * line to set OPT properly */
-// #define OPT 1
-typedef struct __PHONE_BOOK_ENTRY {
+#define OPT 1
+
+#ifndef INDEX_SIZE
+#define INDEX_SIZE 1024
+#endif
+
+typedef struct __PHONE_BOOK_RECORD {
     char lastName[MAX_LAST_NAME_SIZE];
+
     char firstName[16];
     char email[16];
     char phone[10];
@@ -17,10 +23,16 @@ typedef struct __PHONE_BOOK_ENTRY {
     char city[16];
     char state[2];
     char zip[5];
+
+} record;
+typedef struct __PHONE_BOOK_ENTRY {
+    int length;
+    char lastName[INDEX_SIZE][MAX_LAST_NAME_SIZE];
+    record *pRecord[INDEX_SIZE];
     struct __PHONE_BOOK_ENTRY *pNext;
 } entry;
 
-entry *findName(char lastName[], entry *pHead);
+record *findName(char lastName[], entry *pHead);
 entry *append(char lastName[], entry *e);
 
 entry *init();
